6122c8922c21dda89ead82e0ab0bf69ecb9eabac
antoninabagrin <antonia_bagrin@yahoo.com>
2022-07-18 13:07:40 +0300

    services and dependency injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,24 +8,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'A test Recipe',
-      'This is a simply test',
-      'https://149401384.v2.pressablecdn.com/wp-content/uploads/2022/06/summer-ricotta-grilled-vegetables-10-scaled.jpg'
-    ),
-    new Recipe(
-      'Another test Recipe',
-      'This is a simply test',
-      'https://149401384.v2.pressablecdn.com/wp-content/uploads/2022/06/summer-ricotta-grilled-vegetables-10-scaled.jpg'
-    ),
-  ];
-  constructor() {}
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel ',
+      'A super-tasty Schnitzel!!!!!',
+      'https://ro.menulist.menu/storage/media/dishes_main/2220579/conversions/dish_thumbnail.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Burger',
+      'Exactly what you want!!!',
+      'https://www.lalena.ro/images/uploaded/600x_Hamburgeri-de-casa-933.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    // return a new  array which is a copy of one of this file
+    return this.recipes.slice();
+  }
+
+  addIngredientsToSoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
